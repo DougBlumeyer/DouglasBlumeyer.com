b137a6ebff288dc449298f5fd4632fc381534da9
Doug Blumeyer <douglas.blumeyer@gmail.com>
2015-02-19 13:49:58 -0800

    hopefully first theme

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,16 @@
+// ;(function(){
+
+$( document ).ready(function() {
+	
+			// Menu settings
+			$('#menuToggle, .menu-close').on('click', function(){
+				console.log("howdy pahdnah jajaja");
+				// debugger
+				$('#menuToggle').toggleClass('active');
+				$('body').toggleClass('body-push-toleft');
+				$('#theMenu').toggleClass('menu-open');
+			});
+
+
+// })(jQuery)
+});
