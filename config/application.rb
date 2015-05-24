7a4d45bf4fd79d32bd08cebe711bb5d6f2364c42
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2015-05-24 14:04:24 -0700

    revised profile, undid last thing, commented out some bootstrap sprockets thing that might be breaking things and apparently does nothing

--- config/application.rb
+++ config/application.rb
@@ -26,8 +26,5 @@ module DouglasBlumeyerDotCom
     config.action_dispatch.default_headers = {
       'X-Frame-Options' => 'ALLOWALL'
     }
-
-    config.assets.initialize_on_precompile = false
-
   end
 end
