112d1ebd075c0dc62fd7b8dc4004a30e7d8e4410
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2016-04-03 10:33:21 -0700

    gemfile and db minimum

--- db/schema.rb
+++ db/schema.rb
@@ -12,16 +12,4 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20150223021912) do
-
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "webforms", force: true do |t|
-    t.string   "visitor"
-    t.string   "subject"
-    t.text     "message"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end
