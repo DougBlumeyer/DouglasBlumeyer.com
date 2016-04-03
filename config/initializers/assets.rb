c6d35f018d18b1cc55151dc828402b87bb0342f7
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2016-04-03 11:05:49 -0700

    ignore log

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,12 +8,6 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
-# Rails.application.config.assets.precompile += %w( icomoon-free/icomoon.eot )
-# Rails.application.config.assets.precompile += %w( icomoon-free/icomoon.woff )
-# Rails.application.config.assets.precompile += %w( icomoon-free/icomoon.ttf )
-# Rails.application.config.assets.precompile += %w( main.js )
-# Rails.application.config.assets.precompile += %w( Douglas_Blumeyer_2015_resume.pdf )
 Rails.application.config.assets.precompile += %w( thousoondth/basicShapes.js )
 Rails.application.config.assets.precompile += %w( thousoondth/thousoondth.js )
 Rails.application.config.assets.precompile += %w( thousoondth/magicEdge.js )
